2a482181f959da6211e5232bb003b1f1d7923e0f
renan.guedes <r223979@dac.unicamp.br>
2022-04-12 11:12:01 -0300

    angles

--- scratch1.py
+++ scratch1.py
@@ -11,7 +11,13 @@ y2 = float(input("y2: "))
 
 cosAlpha = (x1*x2 + y1*y2)/(((x1**2 + y1**2)**.5)*((x2**2+y2**2)**.5))
 
-if x2 < 0 and y2 < 0:
-    print(acos(cosAlpha) + 2*(180 - acos(cosAlpha)))
+if (x2 >= 0 and y2 >= 0) or (x2 <= 0 and y2 >= 0):
+    print(acos(cosAlpha)*180/pi)
+elif x2 <= 0 and y2 <= 0:
+    print(acos(cosAlpha)*180/pi + 2*(180 - acos(cosAlpha)*180/pi))
+elif x2 >= 0 and y2 <= 0:
+    print(acos(cosAlpha)*180/pi + 360 - 2*acos(cosAlpha)*180/pi)
+else:
+    print("Excecao")
 
-print(acos(cosAlpha)*180/pi)
+
